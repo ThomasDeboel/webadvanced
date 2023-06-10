874774fdbe3618953872c77ad225e60a291d034d
ThomasDeboel <r0841840@student.thomasmore.be>
2023-06-10 20:50:27 +0200

    reaction game + slide show

--- Oefeningen_javascript/src/exercises/es6/functions/reactionGame.js
+++ Oefeningen_javascript/src/exercises/es6/functions/reactionGame.js
@@ -2,7 +2,7 @@
 const delay = 750;
 const blocks = document.querySelectorAll('main div div div');
 let lastNumber = 4;
-let score =0 ;
+let score = 0;
 function randomMinMax10() {
     const min = 0;
     const max = 2;
@@ -12,17 +12,19 @@ function randomMinMax10() {
     return getal;
 }
 function nextBlock() {
-    let number = randomMinMax10();
-    if (number === lastNumber) {
-        number = randomMinMax10();
+    if (score < 5) {
+        let number = randomMinMax10();
+        if (number === lastNumber) {
+            number = randomMinMax10();
+        }
+        blocks.forEach((block) => {
+            block.classList.remove('green');
+            block.classList.remove('red');
+            block.innerText = '';
+        });
+        blocks.item(number).classList.add('red');
+        lastNumber = number;
     }
-    blocks.forEach((block) => {
-        block.classList.remove('green');
-        block.classList.remove('red');
-        block.innerText = '';
-    });
-    blocks.item(number).classList.add('red');
-    lastNumber = number;
 }
 blocks.forEach((block) => {
     block.addEventListener('click', () => {
--- Oefeningen_javascript/src/exercises/es6/functions/slideshow.js
+++ Oefeningen_javascript/src/exercises/es6/functions/slideshow.js
@@ -1,1 +1,14 @@
 // Slideshow
+const ul = document.querySelector('ul');
+
+function toTheBack() {
+    const first = document.querySelector('ul#album li:first-child');
+    const lastitem = document.createElement('li');
+    lastitem.innerHTML = first.innerHTML;
+    ul.append(lastitem);
+    ul.removeChild(first);
+}
+function startgame() {
+    setInterval(toTheBack, 2000);
+}
+startgame();
