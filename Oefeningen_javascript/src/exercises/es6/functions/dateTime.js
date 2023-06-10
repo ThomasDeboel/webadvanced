819ec384aeb85dfe7420fe043f45e821aa20ce0f
ThomasDeboel <r0841840@student.thomasmore.be>
2023-06-10 23:21:26 +0200

    hangman done

--- Oefeningen_javascript/src/exercises/es6/functions/dateTime.js
+++ Oefeningen_javascript/src/exercises/es6/functions/dateTime.js
@@ -1,1 +1,44 @@
 // Date - time
+function clock() {
+    // make new Date-object containing the current date/time
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    let hours = now.getHours(); // convert 24 hours to 12 hours
+    const slider = parseInt(document.getElementById('extra').value, 10);
+    hours += slider;
+    if (hours >= 24) {
+        hours -= 24;
+    }
+    if (hours < 10) {
+        document.getElementById('h0').src = '../../../assets/display/number0.svg';
+    } else {
+        document.getElementById('h0').src = `../../../assets/display/number${(hours - (hours % 10)) / 10}.svg`;
+    }
+    if (minutes < 10) {
+        document.getElementById('m0').src = '../../../assets/display/number0.svg';
+    } else {
+        document.getElementById('m0').src = `../../../assets/display/number${(minutes - (minutes % 10)) / 10}.svg`;
+    }
+    if (seconds < 10) {
+        document.getElementById('s0').src = '../../../assets/display/number0.svg';
+    } else {
+        document.getElementById('s0').src = `../../../assets/display/number${(seconds - (seconds % 10)) / 10}.svg`;
+    }
+    document.getElementById('h1').src = `../../../assets/display/number${hours % 10}.svg`;
+    document.getElementById('m1').src = `../../../assets/display/number${minutes % 10}.svg`;
+    document.getElementById('s1').src = `../../../assets/display/number${seconds % 10}.svg`;
+    if (hours >= 6 && hours < 10) {
+        document.querySelector('h2').innerHTML = 'Good morning';
+        document.querySelector('main div div img').src = '../../../assets/dateTime/morning.png';
+    } else if (hours >= 10 && hours < 21) {
+        document.querySelector('h2').innerHTML = 'Good day';
+        document.querySelector('main div div img').src = '../../../assets/dateTime/day.png';
+    } else if (hours < 6 || hours >= 21) {
+        document.querySelector('h2').innerHTML = 'Good night';
+        document.querySelector('main div div img').src = '../../../assets/dateTime/night.png';
+    }
+}
+clock(); // start clock() immediately
+setInterval(clock, 1000); // execute clock() repeatedly (every second = 1000ms)
--- Oefeningen_javascript/src/exercises/es6/functions/hangman.js
+++ Oefeningen_javascript/src/exercises/es6/functions/hangman.js
@@ -1,3 +1,62 @@
 // Hangman
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const words = ['JAZZ', 'TEST', 'FIVE', 'VISUAL', 'WONDER', 'BEFORE', 'STUDENTS', 'BACHELOR', 'UMBRELLA'];
+
+let aantalGokken = 0;
+let aantalFouten = 0;
+
+for (let i = 0; i < alphabet.length; i++) {
+    document.getElementById('buttonDiv').innerHTML += `<button class="tertiary">${alphabet.charAt(i)}</button>`;
+}
+function randomMinMax() {
+    const getal = Math.floor(Math.random() * (9 + 1));
+    return getal;
+}
+const word = words.at(randomMinMax());
+for (let y = 0; y < word.length; y++) {
+    const span = document.createElement('span');
+    span.innerHTML += `&#160;`;
+    document.getElementById('playDiv').appendChild(span);
+}
+function disableButtons(){
+    document.querySelectorAll('button').forEach((button) => {
+        button.disabled = true;
+    });
+}
+function checkStatus() {
+    if (aantalFouten >= 6) {
+        disableButtons()
+        for (let x = 0; x < word.length; x++) {
+            document.querySelectorAll(`div#playDiv span`)[x].innerHTML = word.charAt(x);
+            document.querySelectorAll(`div#playDiv span`)[x].style.borderColor = 'red';
+            document.querySelectorAll(`div#playDiv span`)[x].style.color = 'red';
+        }
+
+        document.querySelector('figcaption').innerHTML = 'YOU lost :-(';
+    } else if (aantalGokken - aantalFouten === word.length) {
+        disableButtons()
+        document.querySelector('figcaption').innerHTML = 'YOU WON :-)';
+    }
+}
+
+function checkLetter(letter) {
+    aantalGokken++;
+    aantalFouten++;
+    for (let z = 0; z < word.length; z++) {
+        if (word.charAt(z) === letter) {
+            document.querySelectorAll(`div#playDiv span`)[z].innerHTML = letter;
+            document.querySelectorAll(`div#playDiv span`)[z].style.borderColor = 'green';
+            aantalFouten--;
+        }
+    }
+    checkStatus();
+    document.querySelector('figure img').src = `../../../assets/hangman/Hangman-${aantalFouten}.png`;
+}
+
+document.querySelectorAll('button').forEach((button) => {
+    button.addEventListener('click', () => {
+        button.disabled = true;
+        checkLetter(button.innerHTML);
+    });
+});
+console.log(word);
