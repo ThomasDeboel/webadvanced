82b3d5bcee297963df2db90d821071213b67be86
ThomasDeboel <r0841840@student.thomasmore.be>
2023-06-10 16:44:37 +0200

    gallery done

--- Oefeningen_javascript/src/exercises/es6/events/gallery.js
+++ Oefeningen_javascript/src/exercises/es6/events/gallery.js
@@ -1,1 +1,13 @@
 // Image gallery
+const thumbnails = document.querySelectorAll('figure#thumbnails img');
+const image = document.getElementById('fullImage');
+
+thumbnails.forEach((thumbnail) => {
+    thumbnail.addEventListener('click', () => {
+        thumbnails.forEach((restOfThumbnail) => {
+            restOfThumbnail.classList.remove('border');
+        });
+        thumbnail.classList.add('border');
+        image.setAttribute('src', `https://picsum.photos/id/${thumbnail.getAttribute('data-id')}/600/400 `);
+    });
+});
