873ca928bd2dc8f2f54f61cad50f884197a58f0f
ThomasDeboel <r0841840@student.thomasmore.be>
2023-06-10 14:41:53 +0200

    bgcolor advanced

--- Oefeningen_javascript/src/exercises/es6/events/bgcolor_advanced.js
+++ Oefeningen_javascript/src/exercises/es6/events/bgcolor_advanced.js
@@ -1,1 +1,12 @@
 // Change background color (advanced)
+const buttons = document.querySelectorAll('main div button');
+const div = document.querySelector('div.border-gray');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.innerHTML === 'reset') {
+            div.style.backgroundColor = '';
+        }
+        div.style.backgroundColor = button.innerHTML;
+    });
+});
