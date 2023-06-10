e7a866e075fbecdde7f848762aac0ea581832138
ThomasDeboel <r0841840@student.thomasmore.be>
2023-06-10 14:34:58 +0200

    basic background color done

--- Oefeningen_javascript/src/exercises/es6/events/bgcolor_basic.js
+++ Oefeningen_javascript/src/exercises/es6/events/bgcolor_basic.js
@@ -1,1 +1,9 @@
 // Change background color (basic)
+const buttons = document.querySelectorAll('main div button');
+const body = document.querySelector('body');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        body.style.backgroundColor = button.innerHTML;
+    });
+});
